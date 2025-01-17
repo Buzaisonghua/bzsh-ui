a5d262c0517ab918b94e14962adecd1b5e044467
wangbo <99548050@qq.com>
2025-01-17 17:58:21 +0800

    feat: test

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,10 @@
 import eslintConfig from '@bzsh-ui/eslint-config'
 
-export default [...eslintConfig]
+export default [
+  ...eslintConfig,
+  {
+    rules: {
+      // 'import/first': 'off',
+    },
+  },
+]
