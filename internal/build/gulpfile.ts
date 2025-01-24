13328e6de5c94c7f14e0f6df1db46631567ab625
wangbo <99548050@qq.com>
2025-01-24 17:52:44 +0800

    feat: 🚀 组件打包 10%

--- internal/build/gulpfile.ts
+++ internal/build/gulpfile.ts
@@ -1,10 +1,8 @@
-// import { mkdir } from 'fs/promises'
+import { mkdir } from 'fs/promises'
 import { series } from 'gulp'
-// import { epOutput } from '@bzsh-ui/build-utils'
+import { epOutput } from '@bzsh-ui/build-utils'
 import { run, withTaskName } from './src'
-import 'sucrase/register/ts'
-
 export default series(
-  withTaskName('clean', () => run('pnpm run clean'))
-  // withTaskName('createOutput', () => mkdir(epOutput, { recursive: true }))
+  withTaskName('clean', () => run('pnpm run clean')),
+  withTaskName('createOutput', () => mkdir(epOutput, { recursive: true }))
 )
